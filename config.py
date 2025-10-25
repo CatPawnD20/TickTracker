946d16f502d7feed035782aea3f07635562102ac
CatPawnD20 <ata.osman.ozguz@gmail.com>
2025-10-25 07:09:41 +0300

    add .env to .gitignore and load environment variables in config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+﻿from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mt5_login = os.getenv("MT5_LOGIN")
+mt5_password = os.getenv("MT5_PASSWORD")
+mt5_server = os.getenv("MT5_SERVER")
+mt5_path = os.getenv("MT5_PATH")
+
+postgres_host = os.getenv("POSTGRES_HOST")
+postgres_user = os.getenv("POSTGRES_USER")
+postgres_password = os.getenv("POSTGRES_PASSWORD")
+postgres_database = os.getenv("POSTGRES_DATABASE")
